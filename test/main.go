b7dce5622d1cbbaad97f6092ac5a74117ae98a29
Rodolpho Pereira <rodolpho@live.com>
2025-03-26 17:43:12 -0300

    fix: Criar diretório para exportar resultado do teste

--- test/main.go
+++ test/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -15,8 +14,7 @@ type InterSystemsSession struct {
 	command  string
 }
 
-func NewSession() (*InterSystemsSession, error) {
-	// Check for IRIS first, then Cache
+func NewSession() (*InterSystemsSession, error) {	
 	if path, err := exec.LookPath("irissession"); err == nil {
 		return &InterSystemsSession{
 			instance: "IRIS",
@@ -80,114 +78,39 @@ func main() {
 		fmt.Sprintf(`do ##class(%%UnitTest.Manager).RunTest("","/nodelete")`),
 		fmt.Sprintf(`do ##class(%%SYSTEM.Process).Terminate(,0)`),
 	}	
-	
+
 	command := strings.Join(commands, "\n")	
 	exitCode, err := session.ExecuteCommand(command)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error executing commands: %v\n", err)
 		os.Exit(exitCode)
 	}
-	
-	if *generateReport && *outputPath != "" {		
-		if err := os.MkdirAll(filepath.Dir(*outputPath), 0755); err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating output directory: %v\n", err)
-			os.Exit(1)
-		}
 
+	if *generateReport && *outputPath != "" {
 		// Generate JUnit XML report
 		commands := []string{			
 			fmt.Sprintf(`set $namespace = "%s"`, *namespace),
 			fmt.Sprintf(`do ##class(%%File).CreateDirectoryChain("%s")`, *outputPath),
 			fmt.Sprintf(`set File=##class(%%File).%%New("%s/test-report.xml")`, *outputPath),
 			fmt.Sprintf(`set i=$order(^UnitTest.Result(""),-1)`),
-			fmt.Sprintf(`if i="" do ##class(%%SYSTEM.Process).Terminate(,0)`),
+			fmt.Sprintf(`if (i="") { do ##class(%%SYSTEM.Process).Terminate(,0) }`),
 			fmt.Sprintf(`kill ^||TMP`),
-			fmt.Sprintf(`set suite="" for {
-				set suite=$order(^UnitTest.Result(i,suite))
-				quit:suite=""
-				set ^||TMP("S",suite,"time")=$listget(^UnitTest.Result(i,suite),2)
-				set ^||TMP("result")=$listget(^UnitTest.Result(i,suite),1)
-				set case="" for {
-					set case=$order(^UnitTest.Result(i,suite,case))
-					quit:case=""
-					if $increment(^||TMP("S",suite,"tests"))
-					set ^||TMP("S",suite,"C",case,"time")=$listget(^UnitTest.Result(i,suite),2)
-					set method="" for {
-						set method=$order(^UnitTest.Result(i,suite,case,method))
-						quit:method=""
-						if $increment(^||TMP("S",suite,"C",case,"tests"))
-						set ^||TMP("S",suite,"C",case,"M",method,"time")=$listget(^UnitTest.Result(i,suite,case,method),2)
-						set assert="" for {
-							set assert=$order(^UnitTest.Result(i,suite,case,method,assert))
-							quit:assert=""
-							if $increment(^||TMP("S",suite,"assertions"))
-							if $increment(^||TMP("S",suite,"C",case,"assertions"))
-							if $increment(^||TMP("S",suite,"C",case,"M",method,"assertions"))
-							if $listget(^UnitTest.Result(i,suite,case,method,assert))=0 {
-								if $increment(^||TMP("S",suite,"failures"))
-								if $increment(^||TMP("S",suite,"C",case,"failures"))
-								if $increment(^||TMP("S",suite,"C",case,"M",method,"failures"))
-								set ^||TMP("S",suite,"C",case,"M",method,"failure")=$get(^||TMP("S",suite,"C",case,"M",method,"failure"))
-									_$listget(^UnitTest.Result(i,suite,case,method,assert),2)
-									_": "_$listget(^UnitTest.Result(i,suite,case,method,assert),3)
-									_$char(13,10)
-							}
-						}
-					}
-				}
-			}
+			fmt.Sprintf(`set suite="" for { set suite=$order(^UnitTest.Result(i,suite)) quit:(suite="") set ^||TMP("S",suite,"time")=$listget(^UnitTest.Result(i,suite),2) set ^||TMP("result")=$listget(^UnitTest.Result(i,suite),1) set case="" for { set case=$order(^UnitTest.Result(i,suite,case)) quit:(case="") if $increment(^||TMP("S",suite,"tests")) set ^||TMP("S",suite,"C",case,"time")=$listget(^UnitTest.Result(i,suite),2) set method="" for { set method=$order(^UnitTest.Result(i,suite,case,method)) quit:(method="") if $increment(^||TMP("S",suite,"C",case,"tests")) set ^||TMP("S",suite,"C",case,"M",method,"time")=$listget(^UnitTest.Result(i,suite,case,method),2) set assert="" for { set assert=$order(^UnitTest.Result(i,suite,case,method,assert)) quit:(assert="") if $increment(^||TMP("S",suite,"assertions")) if $increment(^||TMP("S",suite,"C",case,"assertions")) if $increment(^||TMP("S",suite,"C",case,"M",method,"assertions")) if $listget(^UnitTest.Result(i,suite,case,method,assert))=0 { if $increment(^||TMP("S",suite,"failures")) if $increment(^||TMP("S",suite,"C",case,"failures")) if $increment(^||TMP("S",suite,"C",case,"M",method,"failures")) set ^||TMP("S",suite,"C",case,"M",method,"failure")=$get(^||TMP("S",suite,"C",case,"M",method,"failure"))_$listget(^UnitTest.Result(i,suite,case,method,assert),2)_": "_$listget(^UnitTest.Result(i,suite,case,method,assert),3)_$char(13,10) } } } } }`),
+			
+			fmt.Sprintf(`do File.Open("WSN")`),
+			fmt.Sprintf(`do File.WriteLine("<?xml version=""1.0"" encoding=""UTF-8"" ?>")`),
+			fmt.Sprintf(`do File.WriteLine("<testsuites>")`),
+			fmt.Sprintf(`set suite="" for { set suite=$order(^||TMP("S",suite)) quit:(suite="") do File.Write("<testsuite") do File.Write(" name="""_$zstrip($zconvert($zconvert(suite,"O","XML"),"O","UTF8"),"<=>W")_"""") do File.Write(" assertions="""_$get(^||TMP("S",suite,"assertions"))_"""") do File.Write(" time="""_$get(^||TMP("S",suite,"time"))_"""") do File.Write(" tests="""_$get(^||TMP("S",suite,"tests"))_"""") do File.WriteLine(">") set case="" for { set case=$order(^||TMP("S",suite,"C",case)) quit:(case="") do File.Write("<testsuite") do File.Write(" name="""_$zstrip($zconvert($zconvert(case,"O","XML"),"O","UTF8"),"<=>W")_"""") do File.Write(" assertions="""_$get(^||TMP("S",suite,"C",case,"assertions"))_"""") do File.Write(" time="""_$get(^||TMP("S",suite,"C",case,"time"))_"""") do File.Write(" tests="""_$get(^||TMP("S",suite,"C",case,"tests"))_"""") do File.WriteLine(">") set method="" for { set method=$order(^||TMP("S",suite,"C",case,"M",method)) quit:(method="") do File.Write("<testcase") do File.Write(" classname="""_$zstrip($zconvert($zconvert(case,"O","XML"),"O","UTF8"),"<=>W")_"""") do File.Write(" name="""_$zstrip($zconvert($zconvert(method,"O","XML"),"O","UTF8"),"<=>W")_"""") do File.Write(" assertions="""_$get(^||TMP("S",suite,"C",case,"M",method,"assertions"))_"""") do File.Write(" time="""_$get(^||TMP("S",suite,"C",case,"M",method,"time"))_"""") do File.WriteLine(">") if $data(^||TMP("S",suite,"C",case,"M",method,"failure")) { do File.Write("<failure type=""cache-error"" message=""Cache Error"">") do File.Write($zconvert($zconvert($zstrip($zstrip(^||TMP("S",suite,"C",case,"M",method,"failure"),"<=>W"),"*E'A'P'N'W"),"O","XML"),"O","UTF8")) do File.WriteLine("</failure>") } do File.WriteLine("</testcase>") } do File.WriteLine("</testsuite>") } do File.WriteLine("</testsuite>") }`),
+			fmt.Sprintf(`do File.WriteLine("</testsuites>")`),
+			fmt.Sprintf(`do File.Close()`),
 
-			do File.Open("WSN")
-			do File.WriteLine("<?xml version=""1.0"" encoding=""UTF-8"" ?>")
-			do File.WriteLine("<testsuites>")
-			set suite="" for {
-				set suite=$order(^||TMP("S",suite))
-				quit:suite=""
-				do File.Write("<testsuite")
-				do File.Write(" name="""_$zstrip($zconvert($zconvert(suite,"O","XML"),"O","UTF8"),"<=>W")_"""")
-				do File.Write(" assertions="""_$get(^||TMP("S",suite,"assertions"))_"""")
-				do File.Write(" time="""_$get(^||TMP("S",suite,"time"))_"""")
-				do File.Write(" tests="""_$get(^||TMP("S",suite,"tests"))_"""")
-				do File.WriteLine(">")
-				set case="" for {
-					set case=$order(^||TMP("S",suite,"C",case))
-					quit:case=""
-					do File.Write("<testsuite")
-					do File.Write(" name="""_$zstrip($zconvert($zconvert(case,"O","XML"),"O","UTF8"),"<=>W")_"""")
-					do File.Write(" assertions="""_$get(^||TMP("S",suite,"C",case,"assertions"))_"""")
-					do File.Write(" time="""_$get(^||TMP("S",suite,"C",case,"time"))_"""")
-					do File.Write(" tests="""_$get(^||TMP("S",suite,"C",case,"tests"))_"""")
-					do File.WriteLine(">")
-					set method="" for {
-						set method=$order(^||TMP("S",suite,"C",case,"M",method))
-						quit:method=""
-						do File.Write("<testcase")
-						do File.Write(" classname="""_$zstrip($zconvert($zconvert(case,"O","XML"),"O","UTF8"),"<=>W")_"""")
-						do File.Write(" name="""_$zstrip($zconvert($zconvert(method,"O","XML"),"O","UTF8"),"<=>W")_"""")
-						do File.Write(" assertions="""_$get(^||TMP("S",suite,"C",case,"M",method,"assertions"))_"""")
-						do File.Write(" time="""_$get(^||TMP("S",suite,"C",case,"M",method,"time"))_"""")
-						do File.WriteLine(">")
-						if $data(^||TMP("S",suite,"C",case,"M",method,"failure")) {
-							do File.Write("<failure type=""cache-error"" message=""Cache Error"">")
-							do File.Write($zconvert($zconvert($zstrip($zstrip(^||TMP("S",suite,"C",case,"M",method,"failure"),"<=>W"),"*E'A'P'N'W"),"O","XML"),"O","UTF8"))
-							do File.WriteLine("</failure>")
-						}
-						do File.WriteLine("</testcase>")
-					}
-					do File.WriteLine("</testsuite>")
-				}
-				do File.WriteLine("</testsuite>")
-			}
-			do File.WriteLine("</testsuites>")
-			do File.Close()
-
-			set result = $get(^||TMP("result"),1)
-			kill ^||TMP
-			if (result=0) { do ##class(%%SYSTEM.Process).Terminate(,1) }
-			do ##class(%%SYSTEM.Process).Terminate(,0)`),
+			fmt.Sprintf(`set result = $get(^||TMP("result"),1)`),
+			fmt.Sprintf(`kill ^||TMP`),
+			fmt.Sprintf(`if (result=0) { do ##class(%%SYSTEM.Process).Terminate(,1) }`),
+			fmt.Sprintf(`do ##class(%%SYSTEM.Process).Terminate(,0)`),
 		}
 		
-		command := strings.Join(commands, "\n")	
+		command := strings.Join(commands, "\n")
 		exitCode, err := session.ExecuteCommand(command)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error generating test report: %v\n", err)
